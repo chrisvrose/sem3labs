a74c753861811f56dfea83814be20f8d3c337580
Christopher Rose <christophervrose@gmail.com>
2019-10-01 13:11:29 +0530

    Dining philosopher: trash output

--- os/05-synch/diner_philosopher.c
+++ os/05-synch/diner_philosopher.c
@@ -8,13 +8,59 @@
 #define SIZE 5
 #define MODADD1(x) (((x)+1)%SIZE)
 
+
+#define MUCHHUNGER 1
+
+//#define
+
 ///Chopsticks
-sem_t chopsticks[SIZE];
+sem_t chopsticks[SIZE],mutex;
 ///State of each philosopher
 char state[SIZE];
+#define THINK 0
+#define HUNGER 1
+#define CONSUME 2
+
+
+void* dinerDoSomething(void* arg){
+	int identity = (int)arg;
+	int outVar1,outVar2;
+	printf("%d\n",identity);
+	for(int i=0;i<MUCHHUNGER;i++){
+		printf("P%d: hunger\n",identity);
+		state[i] = HUNGER;
+		sem_wait(&mutex);
+		sem_getvalue(chopsticks+i,&outVar1);
+		sem_getvalue(chopsticks+MODADD1(i),&outVar2);
+		if(outVar1==1&&outVar2==1){
+			sem_wait(chopsticks+i);
+			sem_wait(chopsticks+MODADD1(i));
+			state[identity]= CONSUME;
+			printf("P%d:consume\n",identity);
+			sem_post(chopsticks+i);
+			sem_post(chopsticks+MODADD1(i));
+			state[identity]=THINK;
+		}
+		sem_post(&mutex);
+	}
+	return NULL;
+}
 
 
 int main(){
-	for(int i=0;i<SIZE;i++) sem_init(chopsticks+i,0,1);
+	sem_init(&mutex,0,1);
+	pthread_t philosopher[SIZE];
+	for(int i=0;i<SIZE;i++){
+		sem_init(chopsticks+i,0,1);
+		state[i] = THINK;
+	}
+	for(int i=0;i<SIZE;i++){
+		pthread_create(philosopher+i,NULL,dinerDoSomething,(void*)i);
+	}
+
+	for(int i=0;i<SIZE;i++){
+		pthread_join(philosopher[i],NULL);
+	}
+
 	return 0;
 }
